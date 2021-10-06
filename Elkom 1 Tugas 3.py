3715dd611e202fda18b3b7eb70c18ba0176c0c0d
Quina12 <91168308+Quina12@users.noreply.github.com>
2021-10-06 12:43:13 +0700

    Add files via upload

---
+++ Elkom 1 Tugas 3.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  6 08:16:06 2021
+
+@author: Quina
+"""
+print(" @@@    @   @  @@@  @    @      @@")
+print("@   @   @   @   @   @@   @     @  @")
+print("@ @ @   @   @   @   @ @  @    @@@@@@")
+print("@   @   @   @   @   @  @ @   @      @")
+print(" @@@ @  @ @ @  @@@  @    @  @        @")
+
+P = int(input("Masukkan bilangan awal : "))
+Q = int(input("Masukkan bilangan akhir : "))
+#hasil = Q > P
+while P and P <= Q :
+    print (P, Q)
+    P += 1
+    Q -= 1
+    
